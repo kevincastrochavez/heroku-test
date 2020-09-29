bf9081cb93abb5e8eb6eb6415927ff66b9802ddc
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-09-28 19:43:45 -0500

    first commit

---
+++ src/components/app.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <h1>This is a Test</h1>
+      </div>
+    );
+  }
+}
